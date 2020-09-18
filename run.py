eb5fdf095d2ea4adcacd98337daf0549034275b8
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-09-18 14:26:51 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,3 +2,13 @@
 import numpy as np
 from lightfm.datasets import fetch_movielens
 from lightfm import LightFM
+# %%
+data = fetch_movielens(min_rating=4.0)
+# %%
+print(repr(data['train']))
+print(repr(data['test']))
+# %%
+model = LightFM(loss='warp')
+# %%
+model.fit(data['train'], epochs=30, num_threads=2)
+# %%
